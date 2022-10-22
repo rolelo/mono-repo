c2621c30d7092981cb4835b0aec02001ebec3086
amirali26 <amiralishojaee123@gmail.com>
2022-10-22 10:36:59 +0100

    moved styles to shared worker

--- packages/organisation/src/App.tsx
+++ packages/organisation/src/App.tsx
@@ -3,9 +3,10 @@ import { ThemeProvider } from '@mui/material';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import './App.css';
 import Routes from './components/routes';
+
 import theme from 'common/static/theme';
+import 'common/styles/App.css';
 
 function App() {
   return (
