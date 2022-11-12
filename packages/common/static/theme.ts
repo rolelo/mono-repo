5c5a870b19ffdc588bc9a3c2307bb3ffb87c9d4a
amirali26 <amiralishojaee123@gmail.com>
2022-11-12 19:25:05 +0000

    updated colours

--- packages/common/static/theme.ts
+++ packages/common/static/theme.ts
@@ -3,8 +3,21 @@ import { createTheme } from '@mui/material';
 const theme = createTheme({
   typography: {
     fontSize: 18,
-  }
+  },
+  palette: {
+    primary: {
+      light: "#33c9dc",
+      main: "#008394",
+      dark: "#008394",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff4569",
+      main: "#b2102f",
+      dark: "#b2102f",
+      contrastText: "#fff",
+    },
+  },
 });
-theme.palette.primary.main = '#313131';
 
 export default theme;
