631cbd20f024a2ccd35f0646884ac1b7059f4fa6
amirali26 <amiralishojaee123@gmail.com>
2023-02-20 23:28:53 +0000

    wip

--- packages/backend/src/elastic/index.ts
+++ packages/backend/src/elastic/index.ts
@@ -2,6 +2,10 @@ import { Client } from "@elastic/elasticsearch";
 
 export const client = new Client({
   node: `${process.env.ELASTIC_URL}:9200`,
+  auth: {
+    username: process.env.ELASTIC_USERNAME,
+    password: process.env.ELASTIC_PASSWORD,
+  },
   tls: {
     ca: process.env.ELASTIC_CA
   }
